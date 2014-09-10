9f4f89e6d75a6e37dd482d4cdadc8668e458f87a
Andre Graf <andre@dergraf.org>
2014-09-10 17:48:05 +0200

    initial commit

---
+++ src/emqttd_passwd.erl
@@ -0,0 +1,111 @@
+-module(emqttd_passwd).
+
+-export([init/0,
+         check/2,
+         hash/1, hash/2,
+         load_from_file/1,
+         load_from_list/1]).
+-export([auth_on_register/4]).
+
+-define(TABLE, ?MODULE).
+-define(SALT_LEN, 12).
+
+-register_hook({auth_on_register, {?MODULE, auth_on_register, 4}}).
+
+init() ->
+    ets:new(?TABLE, [public, named_table, {read_concurrency, true}]),
+    ok.
+
+load_from_file(File) ->
+    age_entries(),
+    {ok, Fd} = file:open(File, [read, binary]),
+    F = fun(FF, read) -> {FF, rl(Fd)};
+           (_, close) -> file:close(Fd)
+        end,
+    parse_passwd_line(F(F,read)),
+    del_aged_entries().
+
+load_from_list(List) ->
+    age_entries(),
+    put(emqttd_passwd_list, List),
+    F = fun(FF, read) ->
+                case get(emqttd_passwd_list) of
+                    [I|Rest] ->
+                        put(emqttd_passwd_list, Rest),
+                        {FF, I};
+                    [] ->
+                        {FF, eof}
+                end;
+           (_, close) ->
+                put(emqttd_passwd_list, undefined),
+                ok
+        end,
+    parse_passwd_line(F(F, read)),
+    del_aged_entries().
+
+check(User, Password) ->
+    case ets:lookup(?TABLE, ensure_binary(User)) of
+        [{_, SaltB64, EncPassword, _}] ->
+            hash(Password, base64:decode(SaltB64)) == EncPassword;
+        [] ->
+            {error, not_found}
+    end.
+
+auth_on_register(_Peer, _ClientId, User, Password) ->
+    case check(User, Password) of
+        true -> ok;
+        false -> {error, invalid_credentials};
+        {error, not_found} -> next
+    end.
+
+parse_passwd_line({F, eof}) ->
+    F(F,close),
+    ok;
+parse_passwd_line({F, <<"\n">>}) ->
+    parse_passwd_line(F(F,read));
+parse_passwd_line({F, Line}) ->
+    [User, Rest] = re:split(Line, ":"),
+    [<<>>, _, Salt, EncPasswd] = binary:split(Rest, <<"$">>, [global]),
+    L = byte_size(EncPasswd) -1,
+    EncPasswdNew =
+    case EncPasswd of
+        <<E:L/binary, "\n">> -> E;
+        _ -> EncPasswd
+    end,
+    Item = {User, Salt, EncPasswdNew, 1},
+    ets:insert(?TABLE, Item),
+    parse_passwd_line(F(F,read)).
+
+age_entries() ->
+    iterate(fun(K) -> ets:update_element(?TABLE, K, {4,2}) end).
+
+del_aged_entries() ->
+    ets:match_delete(?TABLE, {'_', '_', '_', 2}).
+
+iterate(Fun) ->
+    iterate(Fun, ets:first(?TABLE)).
+iterate(_, '$end_of_table') -> ok;
+iterate(Fun, K) ->
+    Fun(K),
+    iterate(Fun, ets:next(?TABLE, K)).
+
+
+rl({ok, Data}) -> Data;
+rl({error, Reason}) -> exit(Reason);
+rl(eof) -> eof;
+rl(Fd) ->
+    rl(file:read_line(Fd)).
+
+ensure_binary(L) when is_list(L) -> list_to_binary(L);
+ensure_binary(B) when is_binary(B) -> B.
+
+
+hash(Password) ->
+    hash(Password, crypto:strong_rand_bytes(?SALT_LEN)).
+
+hash(Password, Salt) ->
+    Ctx1 = crypto:hash_init(sha512),
+    Ctx2 = crypto:hash_update(Ctx1, Password),
+    Ctx3 = crypto:hash_update(Ctx2, Salt),
+    Digest = crypto:hash_final(Ctx3),
+    base64:encode(Digest).
---
+++ src/emqttd_passwd_reloader.erl
@@ -0,0 +1,129 @@
+-module(emqttd_passwd_reloader).
+
+-behaviour(gen_server).
+
+%% API
+-export([start_link/0]).
+
+%% gen_server callbacks
+-export([init/1,
+         handle_call/3,
+         handle_cast/2,
+         handle_info/2,
+         terminate/2,
+         code_change/3]).
+
+-record(state, {file, interval}).
+-define(APP, emqttd_passwd).
+
+%%%===================================================================
+%%% API
+%%%===================================================================
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Starts the server
+%%
+%% @spec start_link() -> {ok, Pid} | ignore | {error, Error}
+%% @end
+%%--------------------------------------------------------------------
+start_link() ->
+    gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
+
+%%%===================================================================
+%%% gen_server callbacks
+%%%===================================================================
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Initializes the server
+%%
+%% @spec init(Args) -> {ok, State} |
+%%                     {ok, State, Timeout} |
+%%                     ignore |
+%%                     {stop, Reason}
+%% @end
+%%--------------------------------------------------------------------
+init([]) ->
+    {ok, File} = application:get_env(?APP, file),
+    {ok, Interval} = application:get_env(?APP, interval),
+    ok = emqttd_passwd:init(),
+    ok = emqttd_passwd:load_from_file(File),
+    erlang:send_after(Interval, self(), reload),
+    {ok, #state{file=File, interval=Interval}}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Handling call messages
+%%
+%% @spec handle_call(Request, From, State) ->
+%%                                   {reply, Reply, State} |
+%%                                   {reply, Reply, State, Timeout} |
+%%                                   {noreply, State} |
+%%                                   {noreply, State, Timeout} |
+%%                                   {stop, Reason, Reply, State} |
+%%                                   {stop, Reason, State}
+%% @end
+%%--------------------------------------------------------------------
+handle_call(_Request, _From, State) ->
+    Reply = ok,
+    {reply, Reply, State}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Handling cast messages
+%%
+%% @spec handle_cast(Msg, State) -> {noreply, State} |
+%%                                  {noreply, State, Timeout} |
+%%                                  {stop, Reason, State}
+%% @end
+%%--------------------------------------------------------------------
+handle_cast(_Msg, State) ->
+    {noreply, State}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Handling all non call/cast messages
+%%
+%% @spec handle_info(Info, State) -> {noreply, State} |
+%%                                   {noreply, State, Timeout} |
+%%                                   {stop, Reason, State}
+%% @end
+%%--------------------------------------------------------------------
+handle_info(reload, #state{file=File, interval=Interval} = State) ->
+    ok = emqttd_passwd:load_from_file(File),
+    erlang:send_after(Interval, self(), reload),
+    {noreply, State}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% This function is called by a gen_server when it is about to
+%% terminate. It should be the opposite of Module:init/1 and do any
+%% necessary cleaning up. When it returns, the gen_server terminates
+%% with Reason. The return value is ignored.
+%%
+%% @spec terminate(Reason, State) -> void()
+%% @end
+%%--------------------------------------------------------------------
+terminate(_Reason, _State) ->
+    ok.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Convert process state when code is changed
+%%
+%% @spec code_change(OldVsn, State, Extra) -> {ok, NewState}
+%% @end
+%%--------------------------------------------------------------------
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
+
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================
