32a1cf0ba37fc3f3471ad0207f5f619fa41b43c9
Andre Graf <andre@dergraf.org>
2014-09-15 10:35:49 +0200

    initial commit

---
+++ src/emqttd_bridge_app.erl
@@ -0,0 +1,16 @@
+-module(emqttd_bridge_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, _StartArgs) ->
+    emqttd_bridge_sup:start_link().
+
+stop(_State) ->
+    ok.
