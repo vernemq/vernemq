4826b03efcb62bd043c5e1b2f83525bab93c9e4f
Andre Graf <andre@dergraf.org>
2014-09-18 11:50:17 +0200

    improved test instrumentation

--- test/emqttd.py
+++ test/emqttd.py
@@ -19,7 +19,8 @@ def adm(broker_path):
 
 
 def start(config_file, broker_path=BROKER_PATH):
-    call([adm(broker_path), "clean"])
+    call([adm(broker_path), "clean-msg-store"], stderr=FNULL)
+    call([adm(broker_path), "clean-registry"], stderr=FNULL)
     ensure_alive(config_file, broker_path)
 
 
@@ -27,26 +28,27 @@ def stop(broker_path=BROKER_PATH):
     pass
 
 def hard_stop(broker_path=BROKER_PATH):
-    call([bin(broker_path), "stop"])
+    call([bin(broker_path), "stop"], stderr=FNULL, stdout=FNULL)
 
 
 
 def node_alive(broker_path):
-    if call([bin(broker_path), "ping"], stdout=FNULL) is 0:
+    if call([bin(broker_path), "ping"], stderr=FNULL, stdout=FNULL) is 0:
         return True
     else:
         return False
 
 
 def ensure_alive(conf_file, broker_path):
-    if not filecmp.cmp(conf_file, config_file(broker_path)):
+    conf_file_differs = not filecmp.cmp(conf_file, config_file(broker_path))
+    if conf_file_differs:
         copyfile(conf_file, config_file(broker_path))
+        hard_stop(broker_path)
 
     start_(broker_path)
 
 def start_(broker_path):
-    print "start broker %s" % broker_path
-    if call([bin(broker_path), "start"]) is 0:
+    if call([bin(broker_path), "start"], stderr=FNULL, stdout=FNULL) is 0:
         if node_alive(broker_path):
             return True
         else:
