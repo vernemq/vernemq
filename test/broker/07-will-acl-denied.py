54039cec9740a97cf9c7e8585520aef0d28d007f
Andre Graf <andre@dergraf.org>
2014-09-18 11:51:06 +0200

    added last will tests

---
+++ test/broker/07-will-acl-denied.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import time
+
+import inspect, os, sys
+# From http://stackoverflow.com/questions/279237/python-import-a-module-from-a-folder
+cmd_subfolder = os.path.realpath(os.path.abspath(os.path.join(os.path.split(inspect.getfile( inspect.currentframe() ))[0],"..")))
+if cmd_subfolder not in sys.path:
+    sys.path.insert(0, cmd_subfolder)
+
+import mosq_test
+import emqttd
+
+rc = 1
+mid = 53
+keepalive = 60
+connect_packet_ok = mosq_test.gen_connect("will-acl-test", keepalive=keepalive, will_topic="ok", will_payload="should be ok")
+connack_packet_ok = mosq_test.gen_connack(rc=0)
+
+connect_packet = mosq_test.gen_connect("will-acl-test", keepalive=keepalive, will_topic="will/acl/test", will_payload="should be denied")
+connack_packet = mosq_test.gen_connack(rc=5)
+
+emqttd.start('07-will-acl-denied.conf')
+
+try:
+    time.sleep(0.5)
+
+    sock_ok = mosq_test.do_client_connect(connect_packet_ok, connack_packet_ok, timeout=5, connack_error="connack ok")
+    sock = mosq_test.do_client_connect(connect_packet, connack_packet)
+    rc = 0
+    sock.close()
+    sock_ok.close()
+finally:
+    emqttd.stop()
+
+exit(rc)
