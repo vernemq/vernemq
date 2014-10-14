db43a19e7a289583bdae7582de054a1d703f5cf1
Andre Graf <andre@dergraf.org>
2014-10-14 15:20:16 +0200

    oops, forgot to solve merge conflict

--- src/vmq_ws.erl
+++ src/vmq_ws.erl
@@ -1,5 +1,4 @@
 %% Copyright 2014 Erlio GmbH Basel Switzerland (http://erl.io)
-<<<<<<< HEAD
 %%
 %% Licensed under the Apache License, Version 2.0 (the "License");
 %% you may not use this file except in compliance with the License.
@@ -7,15 +6,6 @@
 %%
 %%     http://www.apache.org/licenses/LICENSE-2.0
 %%
-=======
-%% 
-%% Licensed under the Apache License, Version 2.0 (the "License");
-%% you may not use this file except in compliance with the License.
-%% You may obtain a copy of the License at
-%% 
-%%     http://www.apache.org/licenses/LICENSE-2.0
-%% 
->>>>>>> 1af0386504e4da4d04eef4f822c204a6ca9572ba
 %% Unless required by applicable law or agreed to in writing, software
 %% distributed under the License is distributed on an "AS IS" BASIS,
 %% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
