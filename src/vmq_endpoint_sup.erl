1af0386504e4da4d04eef4f822c204a6ca9572ba
Andre Graf <andre@dergraf.org>
2014-10-14 10:40:26 +0200

    added copyright, license

--- src/vmq_endpoint_sup.erl
+++ src/vmq_endpoint_sup.erl
@@ -1,11 +1,17 @@
-%%%-------------------------------------------------------------------
-%%% @author graf
-%%% @copyright (C) 2014, graf
-%%% @doc
-%%%
-%%% @end
-%%% Created : 2014-09-03 15:49:38.393546
-%%%-------------------------------------------------------------------
+%% Copyright 2014 Erlio GmbH Basel Switzerland (http://erl.io)
+%%
+%% Licensed under the Apache License, Version 2.0 (the "License");
+%% you may not use this file except in compliance with the License.
+%% You may obtain a copy of the License at
+%%
+%%     http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing, software
+%% distributed under the License is distributed on an "AS IS" BASIS,
+%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%% See the License for the specific language governing permissions and
+%% limitations under the License.
+
 -module(vmq_endpoint_sup).
 -include_lib("public_key/include/public_key.hrl").
 -behaviour(supervisor).
