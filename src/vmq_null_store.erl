4ee450eb46689c694013e1380a583d6a65a6e176
Andre Graf <andre@dergraf.org>
2014-10-28 15:53:49 +0100

    vmq_ets_store is actually not needed since the cache is already holding everything in ETS

--- src/vmq_null_store.erl
+++ src/vmq_null_store.erl
@@ -1,18 +1,18 @@
 %% Copyright 2014 Erlio GmbH Basel Switzerland (http://erl.io)
-%% 
+%%
 %% Licensed under the Apache License, Version 2.0 (the "License");
 %% you may not use this file except in compliance with the License.
 %% You may obtain a copy of the License at
-%% 
+%%
 %%     http://www.apache.org/licenses/LICENSE-2.0
-%% 
+%%
 %% Unless required by applicable law or agreed to in writing, software
 %% distributed under the License is distributed on an "AS IS" BASIS,
 %% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 %% See the License for the specific language governing permissions and
 %% limitations under the License.
 
--module(vmq_ets_store).
+-module(vmq_null_store).
 -behaviour(vmq_msg_store).
 -export([open/1,
          fold/3,
@@ -21,23 +21,13 @@
          close/1]).
 
 open(_Args) ->
-    T = ets:new(?MODULE, [set, named_table]),
-    {ok, T}.
+    {ok, null}.
 
-fold(MsgStoreRef, Fun, Acc) ->
-    ets:foldl(fun({K,V}, A) ->
-                      Fun(K, V, A)
-              end, Acc, MsgStoreRef).
+fold(null, _, Acc) -> Acc.
 
-delete(MsgStoreRef, Key) ->
-    true = ets:delete(MsgStoreRef, Key),
-    ok.
+delete(null, _) -> ok.
 
-insert(MsgStoreRef, Key, Val) ->
-    true = ets:insert(MsgStoreRef, {Key, Val}),
-    ok.
+insert(null, _, _) -> ok.
 
-close(MsgStoreRef) ->
-    true = ets:delete(MsgStoreRef),
-    ok.
+close(null) -> ok.
 
