98e67cb31c3213e68c49e42821f03c527f1efaa8
Andre Graf <andre@dergraf.org>
2017-04-08 12:50:45 +0200

    Fix vmq_admin session show (#346)
    * WIP
    
    * test suite
    
    still WIP
    
    * more tests
    
    * add from to `fold_init_rows`, replace LIKE with MATCH, added proc table, added tests
    
    * Fix multiple filters, add changelog

--- apps/vmq_ql/include/vmq_ql.hrl
+++ apps/vmq_ql/include/vmq_ql.hrl
@@ -1,6 +1,6 @@
 -record(vmq_ql_table, {
           name                  :: atom(),
-          depends_on            :: [#vmq_ql_table{}],
+          depends_on = []       :: [#vmq_ql_table{}],
           provides = []         :: [atom()],
           init_fun              :: function(),
           include_if_all = true :: boolean()
