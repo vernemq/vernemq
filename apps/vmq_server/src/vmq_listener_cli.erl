b8b8238401ff77fd20255bdc2244521ee1d86483
Lars Hesel Christensen <lars@larshesel.dk>
2018-04-17 15:46:12 +0200

    Make it possible to set allowed protocols using the cli

--- apps/vmq_server/src/vmq_listener_cli.erl
+++ apps/vmq_server/src/vmq_listener_cli.erl
@@ -52,6 +52,12 @@ vmq_listener_start_cmd() ->
                                                       {typecast, fun("true") -> true;
                                                                     (_) -> false
                                                                  end}]},
+                 {allowed_protocol_versions, [{longname, "allowed_protocol_versions"},
+                                              {typecast, fun(ProtoVers) ->
+                                                                 {ok, T, _} = erl_scan:string("[" ++ ProtoVers ++ "]."),
+                                                                 {ok, Term} = erl_parse:parse_term(T),
+                                                                 Term
+                                                         end}]},
                  {http, [{shortname, "http"},
                          {longname, "http"}]},
                  {ssl, [{longname, "ssl"}]},
@@ -295,7 +301,9 @@ vmq_listener_start_usage() ->
      "General Options\n\n",
      "  -m, --mountpoint=Mountpoint\n",
      "  --nr_of_acceptors=NrOfAcceptors\n",
-     "  --max_connections=[infinity | MaxConnections]\n\n",
+     "  --max_connections=[infinity | MaxConnections]\n",
+     "  --protocol_versions=[3|4|5]\n",
+     "      Defaults to 3,4\n\n",
      "WebSocket Options\n\n",
      "  --websocket\n",
      "      use the Websocket protocol as the underlying transport\n\n",
