vernemq/vernemq

commit e305f8e8b1deabd5ddee487073563a2fa22feb4f

Plumtree abstraction (#688)
* Add enable_system_pluginn to vmq_plugin_mgr.

This enables to start a plugin without waiting for the registered
process.

* Refactor metadata handling

This commit adds plumtree as a system plugin and refactors the code to
use the vmq_plugin hooks instead of directly calling into
plumtree_metadata.

* Refactor cluster handling

Define new hooks to join and leave a cluster, to rename a node in a
single node cluster, to setup a event handler for cluster membership
events.

* Add vmq_peer_service module to wrap the vmq_plugin:only(cluster*..) calls

* Add changelog

apps/vmq_server/src/vmq_subscriber_db.erl
@@ -26,7 +26,7 @@
 
 -spec store(subscriber_id(), vmq_subscriber:subs()) -> ok.
 store(SubscriberId, Subs) ->
-    plumtree_metadata:put(?SUBSCRIBER_DB, SubscriberId, Subs).
+    vmq_metadata:put(?SUBSCRIBER_DB, SubscriberId, Subs).
 
 -spec read(subscriber_id()) -> undefined |vmq_subscriber:subs().
 read(SubscriberId) ->
@@ -34,7 +34,7 @@ read(SubscriberId) ->
 
 -spec read(subscriber_id(), any()) -> any() |vmq_subscriber:subs().
 read(SubscriberId, Default) ->
-    case plumtree_metadata:get(?SUBSCRIBER_DB, SubscriberId) of
+    case vmq_metadata:get(?SUBSCRIBER_DB, SubscriberId) of
         undefined -> Default;
         Subs ->
             vmq_subscriber:check_format(Subs)
@@ -42,18 +42,16 @@ read(SubscriberId, Default) ->
 
 -spec delete(subscriber_id()) -> ok.
 delete(SubscriberId) ->
-    plumtree_metadata:delete(?SUBSCRIBER_DB, SubscriberId).
+    vmq_metadata:delete(?SUBSCRIBER_DB, SubscriberId).
 
 fold(FoldFun, Acc) ->
-    plumtree_metadata:fold(
-      fun ({_, ?TOMBSTONE}, AccAcc) -> AccAcc;
-          ({SubscriberId, Subs}, AccAcc) ->
+    vmq_metadata:fold(?SUBSCRIBER_DB,
+      fun ({SubscriberId, Subs}, AccAcc) ->
               FoldFun({SubscriberId, vmq_subscriber:check_format(Subs)}, AccAcc)
-      end, Acc, ?SUBSCRIBER_DB,
-      [{resolver, lww}]).
+      end, Acc).
 
 subscribe_db_events() ->
-    plumtree_metadata_manager:subscribe(?SUBSCRIBER_DB),
+    vmq_metadata:subscribe(?SUBSCRIBER_DB),
     fun
         ({deleted, ?SUBSCRIBER_DB, _, Val})
           when (Val == ?TOMBSTONE) or (Val == undefined) ->

apps/vmq_plugin/src/vmq_plugin_mgr.erl
@@ -22,6 +22,7 @@
          enable_plugin/2,
          enable_module_plugin/3,
          enable_module_plugin/4,
+         enable_system_plugin/2,
          disable_plugin/1,
          disable_module_plugin/3,
          disable_module_plugin/4,
@@ -84,6 +85,10 @@ stop() ->
     %% only call after all application that call INTO a
     %% plugin are stopped...
     gen_server:call(?MODULE, stop, infinity).
+
+-spec enable_system_plugin(atom(), [any()]) -> ok | {error, _}.
+enable_system_plugin(Plugin, Opts) ->
+    gen_server:call(?MODULE, {enable_system_plugin, Plugin, Opts}, infinity).
 
 -spec enable_plugin(atom()) -> ok | {error, _}.
 enable_plugin(Plugin) ->
@@ -183,6 +188,9 @@ handle_call(get_usage_lead_lines, _From, #state{plugins=Plugins} = State) ->
           end,
           Plugins),
     {reply, {ok, LeadLines}, State};
+handle_call({enable_system_plugin, Plugin, Opts}, _From, State) ->
+    % system plugins don't have to be deferred
+    handle_plugin_call({enable_plugin, Plugin, Opts}, State);
 handle_call(Call, _From, #state{ready=true} = State) ->
     handle_plugin_call(Call, State);
 handle_call(Call, From, #state{deferred_calls=DeferredCalls} = State) ->
@@ -390,7 +398,7 @@ check_updated_plugins(Plugins, State) ->
             ok = init_plugins_cli(CheckedPlugins),
             ok = start_plugins(CheckedPlugins),
             ok = compile_hooks(CheckedPlugins),
-            {ok, handle_deferred_calls(State#state{ready=true, plugins=CheckedPlugins})};
+            {ok, State#state{plugins=CheckedPlugins}};
         {error, Reason} ->
             {error, Reason}
     end.

apps/vmq_plumtree/.gitignore
@@ -1,0 +1,17 @@
+.rebar3
+_*
+.eunit
+*.o
+*.beam
+*.plt
+*.swp
+*.swo
+.erlang.cookie
+ebin
+log
+erl_crash.dump
+.rebar
+logs
+_build
+.idea
+rebar3.crashdump

apps/vmq_plumtree/rebar.config
@@ -1,0 +1,7 @@
+{erl_opts, [debug_info]}.
+{deps, [
+        %% never heard of plumtree... it is an efficient gossiping protocol 
+        %% bundled with a storage engine. Riak uses a similar mechanism for 
+        %% distributing cluster wide state information.
+        {plumtree, {git, "git://github.com/erlio/plumtree.git", {branch, "master"}}}
+       ]}.

apps/vmq_plumtree/src/vmq_plumtree.app.src
@@ -1,0 +1,33 @@
+{application, vmq_plumtree,
+ [{description, "A VerneMQ plugin that sets up Plumtree as metadata storage"},
+  {vsn, git},
+  {registered, []},
+  {mod, { vmq_plumtree_app, []}},
+  {applications,
+   [kernel,
+    stdlib,
+    plumtree
+   ]},
+  {env,[
+      {vmq_plugin_hooks, [
+            {vmq_plumtree, metadata_put, 3, [internal]},
+            {vmq_plumtree, metadata_get, 2, [internal]},
+            {vmq_plumtree, metadata_delete, 2, [internal]},
+            {vmq_plumtree, metadata_fold, 3, [internal]},
+            {vmq_plumtree, metadata_subscribe, 1, [internal]},
+
+            {vmq_plumtree, cluster_join, 1, [internal]},
+            {vmq_plumtree, cluster_leave, 1, [internal]},
+            {vmq_plumtree, cluster_members, 0, [internal]},
+            {vmq_plumtree, cluster_rename_member, 2, [internal]},
+            {vmq_plumtree, cluster_events_add_handler, 2, [internal]},
+            {vmq_plumtree, cluster_events_delete_handler, 2, [internal]},
+            {vmq_plumtree, cluster_events_call_handler, 3, [internal]}
+      ]}
+  ]},
+  {modules, []},
+
+  {maintainers, []},
+  {licenses, []},
+  {links, []}
+ ]}.

apps/vmq_plumtree/src/vmq_plumtree.erl
@@ -1,0 +1,88 @@
+%% Copyright 2018 Erlio GmbH Basel Switzerland (http://erl.io)
+%%
+%% Licensed under the Apache License, Version 2.0 (the "License");
+%% you may not use this file except in compliance with the License.
+%% You may obtain a copy of the License at
+%%
+%%     http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing, software
+%% distributed under the License is distributed on an "AS IS" BASIS,
+%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%% See the License for the specific language governing permissions and
+%% limitations under the License.
+
+-module(vmq_plumtree).
+-export([metadata_put/3,
+         metadata_get/2,
+         metadata_delete/2,
+         metadata_fold/3,
+         metadata_subscribe/1]).
+
+-export([cluster_join/1,
+         cluster_leave/1,
+         cluster_members/0,
+         cluster_rename_member/2,
+         cluster_events_add_handler/2,
+         cluster_events_delete_handler/2,
+         cluster_events_call_handler/3]).
+
+
+-define(TOMBSTONE, '$deleted').
+
+cluster_join(DiscoveryNode) ->
+    plumtree_peer_service:join(DiscoveryNode).
+
+cluster_leave(Node) ->
+    {ok, Local} = plumtree_peer_service_manager:get_local_state(),
+    {ok, Actor} = plumtree_peer_service_manager:get_actor(),
+    case riak_dt_orswot:update({remove, Node}, Actor, Local) of
+        {error,{precondition,{not_present, Node}}} ->
+            {error, not_present};
+        {ok, Merged} ->
+            _ = gen_server:cast(plumtree_peer_service_gossip, {receive_state, Merged}),
+            {ok, Local2} = plumtree_peer_service_manager:get_local_state(),
+            Local2List = riak_dt_orswot:value(Local2),
+            case [P || P <- Local2List, P =:= Node] of
+                [] ->
+                    ok;
+                _ ->
+                    cluster_leave(Node)
+            end
+    end.
+
+cluster_members() ->
+    {ok, LocalState} = plumtree_peer_service_manager:get_local_state(),
+    riak_dt_orswot:value(LocalState).
+
+cluster_rename_member(OldName, NewName) ->
+    {ok, LocalState} = plumtree_peer_service_manager:get_local_state(),
+    {ok, Actor} = plumtree_peer_service_manager:get_actor(),
+    {ok, Merged} = riak_dt_orswot:update({update, [{remove, OldName},
+                                                   {add, NewName}]}, Actor, LocalState),
+    _ = gen_server:cast(plumtree_peer_service_gossip, {receive_state, Merged}).
+
+cluster_events_add_handler(Module, Opts) ->
+    plumtree_peer_service_events:add_sup_handler(Module, Opts).
+
+cluster_events_delete_handler(Module, Reason) ->
+    gen_event:delete_handler(plumtree_peer_service_events, Module, [Reason]).
+
+cluster_events_call_handler(Module, Msg, Timeout) ->
+    gen_event:call(plumtree_peer_service_events, Module, Msg, Timeout).
+
+metadata_put(FullPrefix, Key, Value) ->
+    plumtree_metadata:put(FullPrefix, Key, Value).
+
+metadata_get(FullPrefix, Key) ->
+    plumtree_metadata:get(FullPrefix, Key).
+
+metadata_delete(FullPrefix, Key) ->
+    plumtree_metadata:delete(FullPrefix, Key).
+
+metadata_fold(FullPrefix, Fun, Acc) ->
+    plumtree_metadata:fold(Fun, Acc, FullPrefix, [{resolver, lww}]).
+
+metadata_subscribe(FullPrefix) ->
+    plumtree_metadata_manager:subscribe(FullPrefix).
+

apps/vmq_plumtree/src/vmq_plumtree_app.erl
@@ -1,0 +1,39 @@
+%% Copyright 2018 Erlio GmbH Basel Switzerland (http://erl.io)
+%%
+%% Licensed under the Apache License, Version 2.0 (the "License");
+%% you may not use this file except in compliance with the License.
+%% You may obtain a copy of the License at
+%%
+%%     http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing, software
+%% distributed under the License is distributed on an "AS IS" BASIS,
+%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%% See the License for the specific language governing permissions and
+%% limitations under the License.
+%%%-------------------------------------------------------------------
+%% @doc vmq_plumtree public API
+%% @end
+%%%-------------------------------------------------------------------
+
+-module(vmq_plumtree_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%%====================================================================
+%% API
+%%====================================================================
+
+start(_StartType, _StartArgs) ->
+    vmq_plumtree_sup:start_link().
+
+%%--------------------------------------------------------------------
+stop(_State) ->
+    ok.
+
+%%====================================================================
+%% Internal functions
+%%====================================================================

apps/vmq_plumtree/src/vmq_plumtree_sup.erl
@@ -1,0 +1,48 @@
+%% Copyright 2018 Erlio GmbH Basel Switzerland (http://erl.io)
+%%
+%% Licensed under the Apache License, Version 2.0 (the "License");
+%% you may not use this file except in compliance with the License.
+%% You may obtain a copy of the License at
+%%
+%%     http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing, software
+%% distributed under the License is distributed on an "AS IS" BASIS,
+%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%% See the License for the specific language governing permissions and
+%% limitations under the License.
+%%%-------------------------------------------------------------------
+%% @doc vmq_plumtree top level supervisor.
+%% @end
+%%%-------------------------------------------------------------------
+
+-module(vmq_plumtree_sup).
+
+-behaviour(supervisor).
+
+%% API
+-export([start_link/0]).
+
+%% Supervisor callbacks
+-export([init/1]).
+
+-define(SERVER, ?MODULE).
+
+%%====================================================================
+%% API functions
+%%====================================================================
+
+start_link() ->
+    supervisor:start_link({local, ?SERVER}, ?MODULE, []).
+
+%%====================================================================
+%% Supervisor callbacks
+%%====================================================================
+
+%% Child :: {Id,StartFunc,Restart,Shutdown,Type,Modules}
+init([]) ->
+    {ok, { {one_for_all, 0, 1}, []} }.
+
+%%====================================================================
+%% Internal functions
+%%====================================================================

commit ec90a16e21409a94109469af3e1fa8a6e2fd3d6a

Fix regression introduced by #688
Remember to filter out deleted metadata a.k.a tombstones.

apps/vmq_server/src/vmq_subscriber_db.erl
@@ -46,7 +46,8 @@ delete(SubscriberId) ->
 
 fold(FoldFun, Acc) ->
     vmq_metadata:fold(?SUBSCRIBER_DB,
-      fun ({SubscriberId, Subs}, AccAcc) ->
+      fun ({_, ?TOMBSTONE}, AccAcc) -> AccAcc;
+          ({SubscriberId, Subs}, AccAcc) ->
               FoldFun({SubscriberId, vmq_subscriber:check_format(Subs)}, AccAcc)
       end, Acc).
 