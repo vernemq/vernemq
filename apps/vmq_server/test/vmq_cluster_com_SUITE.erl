e2530b00e069fc8b1149532781be4877114aaa20
Andre Graf <andre@dergraf.org>
2018-11-09 14:45:11 +0100

    Improve test cases, add changelog

--- apps/vmq_server/test/vmq_cluster_com_SUITE.erl
+++ apps/vmq_server/test/vmq_cluster_com_SUITE.erl
@@ -37,7 +37,8 @@ end_per_testcase(_Case, _Config) ->
 
 all() ->
     [connect_success_test,
-     connect_success_send_error
+     connect_success_send_error,
+     connect_success_send_error_timeout
     ].
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -53,7 +54,7 @@ connect_success_test(Config) ->
     recv_connect(Socket, Config),
 
     % send test message
-    send_message(ClusterNodePid, hello_world),
+    ok = send_message(ClusterNodePid, hello_world),
     % recv this message
     recv_message(Socket, hello_world).
 
@@ -66,12 +67,56 @@ connect_success_send_error(Config) ->
     % close this socket
     erlang:port_close(Socket1),
     % send test message, will be buffered and delivered on next successful reconnect
-    send_message(ClusterNodePid, hello_world),
+    ok = send_message(ClusterNodePid, hello_world),
 
     {ok, Socket2} = gen_tcp:accept(ListenSocket, 20000),
     recv_connect(Socket2, Config),
     % recv this message
     recv_message(Socket2, hello_world).
+
+connect_success_send_error_timeout(Config) ->
+    % check that message isn't lost
+    ClusterNodePid = setup_mock_vmq_cluster_node(Config),
+    {ok, ListenSocket} = gen_tcp:listen(12345, [binary, {reuseaddr, true}, {active, false}]),
+    {ok, Socket1} = gen_tcp:accept(ListenSocket, 20000),
+    recv_connect(Socket1, Config),
+
+    N = send_until_tcp_buffer_full(ClusterNodePid),
+    % once the tcp buffer is full, we get disconnected
+    recv_until_tcp_buffer_empty(Socket1, N),
+    % we should have a TCP_CLOSE now
+    {error, closed} = gen_tcp:recv(Socket1, 0),
+
+    % the cluster node should do the reconnect
+    {ok, Socket2} = gen_tcp:accept(ListenSocket, 20000),
+    recv_connect(Socket2, Config),
+
+    % the last buffered message is repeated as the cluster node doesn't
+    % actually know if we have received it or not.
+    recv_message(Socket2, <<1:10000, N:32>>),
+    {error, timeout} = gen_tcp:recv(Socket2, 0, 1000).
+
+
+send_until_tcp_buffer_full(ClusterNodePid) ->
+   send_until_tcp_buffer_full(ClusterNodePid, 0).
+send_until_tcp_buffer_full(ClusterNodePid, MsgsAcc) ->
+    % the only way we detect that the buffer is full is that vmq_cluster_node will close
+    % the connection and will reconnect
+    case send_message(ClusterNodePid, <<1:10000, MsgsAcc:32>>) of
+        ok  ->
+            send_until_tcp_buffer_full(ClusterNodePid, MsgsAcc + 1);
+        {error, msg_dropped} ->
+            MsgsAcc - 1
+    end.
+
+recv_until_tcp_buffer_empty(Socket, N) ->
+    recv_until_tcp_buffer_empty(Socket, 0, N).
+
+recv_until_tcp_buffer_empty(Socket, I, N) when I =< N ->
+    recv_message(Socket, <<1:10000, I:32>>),
+    recv_until_tcp_buffer_empty(Socket, I + 1, N);
+recv_until_tcp_buffer_empty(_, _, _) -> ok.
+
 
 setup_mock_vmq_cluster_node(Config) ->
     setup_mock_vmq_cluster_node(Config, []).
@@ -82,6 +127,7 @@ setup_mock_vmq_cluster_node(Config, Opts) ->
     ok = rpc:call(Node, vmq_config, set_env, [outgoing_connect_opts, Opts, false]),
     ok = rpc:call(Node, vmq_config, set_env, [outgoing_connect_params_module, ?MODULE, false]),
     ok = rpc:call(Node, vmq_config, set_env, [outgoing_connect_timeout, 1000, false]),
+    ok = rpc:call(Node, vmq_config, set_env, [outgoing_clustering_buffer_size, 1000, false]),
     {ok, ClusterNodePid} = rpc:call(Node, vmq_cluster_node, start_link, [node()]),
     ClusterNodePid.
 
@@ -95,11 +141,16 @@ recv_connect(Socket, Config) ->
 
 
 send_message(ClusterNodePid, Msg) ->
-    ok = rpc:call(node(ClusterNodePid), vmq_cluster_node, publish, [ClusterNodePid, Msg]).
+    rpc:call(node(ClusterNodePid), vmq_cluster_node, publish, [ClusterNodePid, Msg]).
 
 recv_message(Socket, Term) ->
     TermBin = term_to_binary(Term),
     L = byte_size(TermBin),
     Msg = <<"msg", L:32, TermBin/binary>>,
     BatchMsg = <<"vmq-send", (byte_size(Msg)):32, Msg/binary>>,
-    {ok, BatchMsg} = gen_tcp:recv(Socket, byte_size(BatchMsg)).
+    case gen_tcp:recv(Socket, byte_size(BatchMsg)) of
+        {ok, BatchMsg} -> ok;
+        E ->
+            io:format(user, "got ~p instead of ~p~n", [E, {ok, BatchMsg}]),
+            E
+    end.
