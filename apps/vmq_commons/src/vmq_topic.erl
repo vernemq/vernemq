e80fb68709a5e8941e03a772a822ea1052cc4d02
Andre Graf <andre@dergraf.org>
2019-08-28 16:36:46 +0200

    Fix license header

--- apps/vmq_commons/src/vmq_topic.erl
+++ apps/vmq_commons/src/vmq_topic.erl
@@ -1,16 +1,17 @@
-%% The contents of this file are subject to the Mozilla Public License
-%% Version 1.1 (the "License"); you may not use this file except in
-%% compliance with the License. You may obtain a copy of the License
-%% at http://www.mozilla.org/MPL/
+
+%% Copyright 2019 Octavo Labs AG Zurich Switzerland (https://octavolabs.com)
 %%
-%% Software distributed under the License is distributed on an "AS IS"
-%% basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
-%% the License for the specific language governing rights and
+%% Licensed under the Apache License, Version 2.0 (the "License");
+%% you may not use this file except in compliance with the License.
+%% You may obtain a copy of the License at
+%%
+%%     http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing, software
+%% distributed under the License is distributed on an "AS IS" BASIS,
+%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%% See the License for the specific language governing permissions and
 %% limitations under the License.
-%%
-%% Developer of the eMQTT Code is <ery.lee@gmail.com>
-%% Copyright (c) 2012 Ery Lee.  All rights reserved.
-%%
 -module(vmq_topic).
 
 -import(lists, [reverse/1]).
