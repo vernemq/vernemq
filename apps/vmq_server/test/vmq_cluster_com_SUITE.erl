4774cc4eb9527b6b78d8f1458b151d7b89c08912
user.email <dairon.medina@gmail.com>
2020-06-16 09:57:46 -0400

    Cleanup and use released bcrypt package

--- apps/vmq_server/test/vmq_cluster_com_SUITE.erl
+++ apps/vmq_server/test/vmq_cluster_com_SUITE.erl
@@ -18,7 +18,6 @@ init_per_suite(Config) ->
     end,
     ct:log("node name ~p", [node()]),
     Node = vmq_cluster_test_utils:start_node(test_com1, Config, default_case),
-    ok = rpc:call(Node, vmq_auth, register_hooks, []),
     {ok, _} = ct_cover:add_nodes([Node]),
     vmq_cluster_test_utils:wait_until_ready([Node]),
     [{node, Node}, S|Config].
