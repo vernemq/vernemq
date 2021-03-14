ef39f4fcd147436bdc58af20c94d4b30fd4c254a
widhaprasa <widhaprasa@gmail.com>
2021-03-15 00:50:44 +0700

    - Update mysql lua rule

--- apps/vmq_diversity/priv/auth/mysql.lua
+++ apps/vmq_diversity/priv/auth/mysql.lua
@@ -1,72 +1,39 @@
--- Copyright 2018 Erlio GmbH Basel Switzerland (http://erl.io)
---
--- Licensed under the Apache License, Version 2.0 (the "License");
--- you may not use this file except in compliance with the License.
--- You may obtain a copy of the License at
---
---     http://www.apache.org/licenses/LICENSE-2.0
---
--- Unless required by applicable law or agreed to in writing, software
--- distributed under the License is distributed on an "AS IS" BASIS,
--- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
--- See the License for the specific language governing permissions and
--- limitations under the License.
+function cache_insert(mountpoint, username, publish_acl, subscribe_acl)
+    type_assert(mountpoint, "string", "mountpoint")
+    type_assert(username, "string", "username")
+    type_assert(publish_acl, {"table", "nil"}, "publish_acl")
+    type_assert(subscribe_acl, {"table", "nil"}, "subscribe_acl")
+    validate_acls(publish_acl)
+    validate_acls(subscribe_acl)
+    auth_cache.insert(mountpoint, username, username, publish_acl, subscribe_acl)
+end
 
--- MySQL Configuration, read the documentation below to properly
--- provision your database.
-require "auth/auth_commons"
+function validate_acls(acls) 
+    if acls ~= nil then
+        for i, acl in ipairs(acls) do
+            for k, v in pairs(acl) do
+                type_assert(k, "string", "acl key")
+                if k == "modifiers" then
+                    type_assert(v, "table", "acl modifiers")
+                    -- TODO validate modifier types
+                elseif  k == "pattern" then
+                    type_assert(v, "string", "acl pattern")
+                else
+                    type_assert(v, {"string", "number", "boolean"}, "acl value")
+                end
+            end
+        end
+    end
+end
 
--- In order to use this Lua plugin you must deploy the following database
--- schema and grant the user configured above with the required privileges:
---[[ 
-   CREATE TABLE vmq_auth_acl
-   (
-     mountpoint VARCHAR(10) NOT NULL,
-     client_id VARCHAR(128) NOT NULL,
-     username VARCHAR(128) NOT NULL,
-     password VARCHAR(128),
-     publish_acl TEXT,
-     subscribe_acl TEXT,
-     CONSTRAINT vmq_auth_acl_primary_key PRIMARY KEY (mountpoint, client_id, username)
-   )
-  ]] --
--- To insert a client ACL use a similar SQL statement:
--- NOTE THAT `PASSWORD()` NEEDS TO BE SUBSTITUTED ACCORDING TO THE HASHING METHOD
--- CONFIGURED IN `vmq_diversity.mysql.password_hash_method`. CHECK THE MYSQL DOCS TO 
--- FIND THE MATCHING ONE AT https://dev.mysql.com/doc/refman/8.0/en/encryption-functions.html.
---
--- 
---[[
---
-   INSERT INTO vmq_auth_acl 
-   (mountpoint, client_id, username, 
-    password, publish_acl, subscribe_acl)
- VALUES 
-   ('', 'test-client', 'test-user', 
-    PASSWORD('123'), '[{"pattern":"a/b/c"},{"pattern":"c/b/#"}]', 
-                     '[{"pattern":"a/b/c"},{"pattern":"c/b/#"}]');
-
-]]--
--- 	The JSON array passed as publish/subscribe ACL contains the topic patterns
--- 	allowed for this particular user. MQTT wildcards as well as the variable 
--- 	substitution for %m (mountpoint), %c (client_id), %u (username) are allowed
--- 	inside a pattern. 
---
--- 
--- IF YOU USE THE SCHEMA PROVIDED ABOVE NOTHING HAS TO BE CHANGED IN THE
--- FOLLOWING SCRIPT.
 function auth_on_register(reg)
     if reg.username ~= nil and reg.password ~= nil then
         results = mysql.execute(pool,
             [[SELECT publish_acl, subscribe_acl
               FROM vmq_auth_acl
               WHERE
-                mountpoint=? AND
-                client_id=? AND
                 username=? AND
                 password=]]..mysql.hash_method(),
-            reg.mountpoint,
-            reg.client_id,
             reg.username,
             reg.password)
         if #results == 1 then
@@ -74,8 +41,7 @@ function auth_on_register(reg)
             publish_acl = json.decode(row.publish_acl)
             subscribe_acl = json.decode(row.subscribe_acl)
             cache_insert(
-                reg.mountpoint, 
-                reg.client_id, 
+                reg.mountpoint,
                 reg.username,
                 publish_acl,
                 subscribe_acl
