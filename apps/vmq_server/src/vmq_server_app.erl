7d2ad0444f4aa09c3629850fa5246d529ef2f58f
Andre Fatton <afa@erl.io>
2023-04-05 16:18:56 +0200

    Maybe update nodetool script during boot to adapt for distribution protocol.

--- apps/vmq_server/src/vmq_server_app.erl
+++ apps/vmq_server/src/vmq_server_app.erl
@@ -27,7 +27,7 @@
 start(_StartType, _StartArgs) ->
     ok = vmq_metadata:start(),
     ok = vmq_message_store:start(),
-
+    maybe_update_nodetool(),
     case vmq_server_sup:start_link() of
         {error, _} = E ->
             E;
@@ -72,3 +72,26 @@ stop(_State) ->
     _ = vmq_message_store:stop(),
     _ = vmq_metadata:stop(),
     ok.
+
+maybe_update_nodetool() ->
+    case init:get_argument(proto_dist) of
+        {ok, [[ProtoDist]]} ->
+            Root = code:root_dir(),
+            Nodetool = filename:join([
+                Root, "erts-" ++ erlang:system_info(version), "bin", "nodetool"
+            ]),
+            {ok, [Shebang, Comment, _, Source]} = escript:extract(Nodetool, []),
+            {ok, UpdatedScriptBin} =
+                escript:create(binary, [
+                    Shebang, Comment, {emu_args, "+fnu -proto_dist " ++ ProtoDist}, Source
+                ]),
+            try file:write_file(Nodetool, UpdatedScriptBin) of
+                ok -> ok
+            catch
+                E:R ->
+                    lager:info("Could not update nodetool due to ~p for reason ~p ~n", [E, R]),
+                    {error, R}
+            end;
+        error ->
+            ignore
+    end.
