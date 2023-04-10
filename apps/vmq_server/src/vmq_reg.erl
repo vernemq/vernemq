74ef62f36b2e4fe682d401fbd2f9f99efaa6e7f8
ioolkos <afa@erl.io>
2023-04-10 14:57:26 +0200

    Fix mountpoint filtering for vmq-admin retain show. Add delete command. (#2135)
    * Fix mountpoint filtering for You need to be root or use sudo to run this command. Add delete command.
    
    * Fix mountpoint filtering for You need to be root or use sudo to run this command. Add delete command.
    
    * retain delete: clarification on help doc string
    
    * Add simple test for administrative retain delete

--- apps/vmq_server/src/vmq_reg.erl
+++ apps/vmq_server/src/vmq_reg.erl
@@ -587,7 +587,7 @@ deliver_retained({MP, _} = SubscriberId, Topic, QoS, SubOpts, _) ->
     vmq_retain_srv:match_fold(
         fun
             (
-                {T, #retain_msg{
+                {{_M, T}, #retain_msg{
                     payload = Payload,
                     properties = Properties,
                     expiry_ts = ExpiryTs
@@ -608,7 +608,7 @@ deliver_retained({MP, _} = SubscriberId, Topic, QoS, SubOpts, _) ->
                 Msg1 = maybe_add_sub_id({QoS, SubOpts}, Msg),
                 maybe_delete_expired(ExpiryTs, MP, Topic),
                 vmq_queue:enqueue(QPid, {deliver, QoS, Msg1});
-            ({T, Payload}, _) when is_binary(Payload) ->
+            ({{_M, T}, Payload}, _) when is_binary(Payload) ->
                 %% compatibility with old style retained messages.
                 Msg = #vmq_msg{
                     routing_key = T,
