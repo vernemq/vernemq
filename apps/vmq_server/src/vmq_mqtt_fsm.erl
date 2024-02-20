8352c4bcce130d26ec94232d334a19102c29408b
Dhruv Jain <51796498+dhruvjain99@users.noreply.github.com>
2024-02-20 21:58:53 +0530

    fix: warning level for unexpected frame log (#46)

--- apps/vmq_server/src/vmq_mqtt_fsm.erl
+++ apps/vmq_server/src/vmq_mqtt_fsm.erl
@@ -567,7 +567,7 @@ connected(close_timeout, State) ->
     %% As we're in the connected state, it's ok to ignore this timeout
     {State, []};
 connected(Unexpected, State) ->
-    lager:warning("stopped connected session, due to unexpected frame type ~p", [Unexpected]),
+    lager:error("stopped connected session, due to unexpected frame type ~p", [Unexpected]),
     terminate({error, unexpected_message, Unexpected}, State).
 
 connack_terminate(RC, _State) ->
