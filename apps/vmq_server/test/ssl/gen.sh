153baf0bb9c9a790eee31dc3590d1948b00cef71
ioolkos <afa@octavolabs.com>
2026-07-18 14:39:44 +0200

    Update SSL certs to pass stricter checks of SubjectAltName (#2516)

--- apps/vmq_server/test/ssl/gen.sh
+++ apps/vmq_server/test/ssl/gen.sh
@@ -31,45 +31,46 @@ openssl req -new -x509 -days 3650 -key test-fake-root-ca.key -out test-fake-root
 # An intermediate CA, signed by the root CA, used to sign server/client csrs.
 openssl genrsa -out test-signing-ca.key 2048
 openssl req -out test-signing-ca.csr -key test-signing-ca.key -new -config openssl.cnf -subj "${BASESUBJ}/CN=Signing CA/"
-openssl ca -config openssl.cnf -name CA_root -extensions v3_ca -out test-signing-ca.crt -infiles test-signing-ca.csr
+openssl ca -batch -config openssl.cnf -name CA_root -extensions v3_ca -out test-signing-ca.crt -infiles test-signing-ca.csr
 
 # An alternative intermediate CA, signed by the root CA, not used to sign anything.
 openssl genrsa -out test-alt-ca.key 2048
 openssl req -out test-alt-ca.csr -key test-alt-ca.key -new -config openssl.cnf -subj "${BASESUBJ}/CN=Alternative Signing CA/"
-openssl ca -config openssl.cnf -name CA_root -extensions v3_ca -out test-alt-ca.crt -infiles test-alt-ca.csr
+openssl ca -batch -config openssl.cnf -name CA_root -extensions v3_ca -out test-alt-ca.crt -infiles test-alt-ca.csr
 
 # Valid server key and certificate.
 openssl genrsa -out server.key 2048
 openssl req -new -key server.key -out server.csr -config openssl.cnf -subj "${SBASESUBJ}/CN=localhost/"
-openssl ca -config openssl.cnf -name CA_signing -out server.crt -infiles server.csr
+openssl ca -batch -config openssl.cnf -name CA_signing -extensions server_cert_localhost -out server.crt -infiles server.csr
+openssl rsa -aes256 -in server.key -out server.encrypted.key -passout pass:VerneMQ123
 
 # Valid server key and certificate with wrong CN.
 openssl genrsa -out bad-cn-server.key 2048
 openssl req -new -key bad-cn-server.key -out bad-cn-server.csr -config openssl.cnf -subj "${SBASESUBJ}/CN=notlocalhost/"
-openssl ca -config openssl.cnf -name CA_signing -out bad-cn-server.crt -infiles bad-cn-server.csr
+openssl ca -batch -config openssl.cnf -name CA_signing -extensions server_cert_notlocalhost -out bad-cn-server.crt -infiles bad-cn-server.csr
 
 # Expired server certificate, based on the above server key.
 openssl req -new -days 1 -key server.key -out server-expired.csr -config openssl.cnf -subj "${SBASESUBJ}/CN=localhost/"
-openssl ca -config openssl.cnf -name CA_signing -days 1 -startdate 120820000000Z -enddate 120821000000Z -out server-expired.crt -infiles server-expired.csr
+openssl ca -batch -config openssl.cnf -name CA_signing -extensions server_cert_localhost -days 1 -startdate 120820000000Z -enddate 120821000000Z -out server-expired.crt -infiles server-expired.csr
 
 # Valid client key and certificate.
 openssl genrsa -out client.key 2048
 openssl req -new -key client.key -out client.csr -config openssl.cnf -subj "${SBASESUBJ}/CN=test client/"
-openssl ca -config openssl.cnf -name CA_signing -out client.crt -infiles client.csr
+openssl ca -batch -config openssl.cnf -name CA_signing -out client.crt -infiles client.csr
 
 # Expired client certificate, based on the above client key.
 openssl req -new -days 1 -key client.key -out client-expired.csr -config openssl.cnf -subj "${SBASESUBJ}/CN=test client expired/"
-openssl ca -config openssl.cnf -name CA_signing -days 1 -startdate 120820000000Z -enddate 120821000000Z -out client-expired.crt -infiles client-expired.csr
+openssl ca -batch -config openssl.cnf -name CA_signing -days 1 -startdate 120820000000Z -enddate 120821000000Z -out client-expired.crt -infiles client-expired.csr
 
 # Revoked client certificate, based on a new client key.
 openssl genrsa -out client-revoked.key 2048
 openssl req -new -days 1 -key client-revoked.key -out client-revoked.csr -config openssl.cnf -subj "${SBASESUBJ}/CN=test client revoked/"
-openssl ca -config openssl.cnf -name CA_signing -out client-revoked.crt -infiles client-revoked.csr
-openssl ca -config openssl.cnf -name CA_signing -revoke client-revoked.crt
-openssl ca -config openssl.cnf -name CA_signing -gencrl -out crl.pem
+openssl ca -batch -config openssl.cnf -name CA_signing -out client-revoked.crt -infiles client-revoked.csr
+openssl ca -batch -config openssl.cnf -name CA_signing -revoke client-revoked.crt
+openssl ca -batch -config openssl.cnf -name CA_signing -gencrl -out crl.pem
 
 cat test-signing-ca.crt test-root-ca.crt > all-ca.crt
-#mkdir certs
+mkdir certs
 #cp test-signing-ca.crt certs/test-signing-ca.pem
 #cp test-root-ca.crt certs/test-root.ca.pem
 c_rehash certs
